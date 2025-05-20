f13d0173458f55fc4d034e56fd2e7f10b8a27e40
John St Hilaire <84206822+neotions@users.noreply.github.com>
2025-05-20 16:38:14 -0400

    testing some new tool use

---
+++ lib/ai/tools/make-chart.ts
@@ -0,0 +1,60 @@
+import { tool } from 'ai';
+import { z } from 'zod';
+import { Utils } from 'chart.js';
+
+export const barChart = tool({
+  description: 'Get the current weather at a location, give your answer in Fahrenheit if in the US, Celcius if anywhere else.',
+  parameters: z.object({
+    latitude: z.number(),
+    longitude: z.number(),
+  }),
+  execute: async ({ latitude, longitude }) => {
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+Chart
+    return marsWeatherData;
+  },
+});
+
+
+
+
+
